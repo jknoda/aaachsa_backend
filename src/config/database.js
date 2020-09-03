4276bd0ccea926f95de2e8fb5099693cf6d68cd8
jknoda <jknoda@gmail.com>
2020-09-03 16:15:36 -0300

    ajustes

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
     dialect: 'mysql',
-    host: '192.168.1.244',
-    username: 'aaachsa',
-    password: 'senha',
-    database: 'dbaaachsa',
+    host: 'us-cdbr-east-02.cleardb.com', //'192.168.1.244',
+    username: 'bfd835e32c697e', //'aaachsa',
+    password: 'de4bd86a', //'senha',
+    database: 'heroku_4fd15084841e4fa', //'dbaaachsa',
     define: {
         timestamps: false
     }
